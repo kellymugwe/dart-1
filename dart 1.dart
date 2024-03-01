9e3ef8eea02631d2de45349de2fc0d0fd3206561
kellymugwe <114218602+kellymugwe@users.noreply.github.com>
2024-03-01 15:12:53 +0300

    Add files via upload

---
+++ dart 1.dart
@@ -0,0 +1,27 @@
+void main() {
+  // Demonstration of int data type
+  int age = 26;
+
+  // Demonstration of double data type
+  double weight = 75.5;
+
+  // Demonstration of String data type
+  String name = 'John ky';
+
+  // Demonstration of List data type (dynamic)
+  List<dynamic> fruits = ['pawpaw', 'peach', 'orange'];
+
+  // Demonstration of Map data type
+  Map<String, int> studentScores = {
+    'kelly': 40,
+    'john': 80,
+    'marry': 60,
+  };
+
+  // Printing values
+  print('Age: $age');
+  print('Weight: $weight');
+  print('Name: $name');
+  print('Fruits: $fruits');
+  print('Student Scores: $studentScores');
+}
